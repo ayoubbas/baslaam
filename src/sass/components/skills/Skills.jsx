bd4b43025f4dbe9e7e7b1a724a0232359cab957b
ayoubbas <ayoub-ok147@hotmail.fr>
2024-12-24 18:26:35 +0100

    update today

--- src/sass/components/skills/Skills.jsx
+++ src/sass/components/skills/Skills.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HiBadgeCheck } from "react-icons/hi";
 
 const Skills = () => {
   return (
@@ -9,8 +10,61 @@ const Skills = () => {
           <p className="section__subtitle">My technical level</p>
         </div>
         <div className="skills__content">
-          <div className="skills__content--frontend"></div>
-          <div className="skills__content--backend"></div>
+          <div className="skills__content--frontend">
+            <h2 className="skills__title">Frontend developer</h2>
+            <div className="skills-lists">
+              <ul className="skills-list">
+                <li className="skills-item">
+                  <HiBadgeCheck className="skills-icon" />
+                  <div className="skill-title">
+                    <p className="skill-name">HTML</p>
+                    <p className="skill-level">Basic</p>
+                  </div>
+                </li>
+                <li className="skills-item">
+                  <HiBadgeCheck className="skills-icon" />
+
+                  <div className="skill-title">
+                    <p className="skill-name">CSS</p>
+                    <p className="skill-level">Advanced</p>
+                  </div>
+                </li>
+                <li className="skills-item">
+                  <HiBadgeCheck className="skills-icon" />
+                  <div className="skill-title">
+                    <p className="skill-name">JavaScript</p>
+                    <p className="skill-level">Intermediate</p>
+                  </div>
+                </li>
+              </ul>
+              <ul className="skills-list">
+                <li className="skills-item">
+                  <HiBadgeCheck className="skills-icon" />
+                  <div className="skill-title">
+                    <p className="skill-name">Bootstrap</p>
+                    <p className="skill-level">Intermediate</p>
+                  </div>
+                </li>
+                <li className="skills-item">
+                  <HiBadgeCheck className="skills-icon" />
+                  <div className="skill-title">
+                    <p className="skill-name">Git</p>
+                    <p className="skill-level">Intermediate</p>
+                  </div>
+                </li>
+                <li className="skills-item">
+                  <HiBadgeCheck className="skills-icon" />
+                  <div className="skill-title">
+                    <p className="skill-name">React</p>
+                    <p className="skill-level">Intermediate</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="skills__content--backend">
+            <h2 className="skills__title">Backend developer</h2>
+          </div>
         </div>
       </div>
     </section>
