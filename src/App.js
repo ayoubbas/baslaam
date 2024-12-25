00ac55f3c2c5f34020061249d03bce2b2014ae3d
ayoubbas <ayoub-ok147@hotmail.fr>
2024-12-25 15:47:32 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./sass/_main.scss";
 import Home from "./sass/pages/home/Home";
 import About from "./sass/components/about/about/About";
 import Skills from "./sass/components/skills/Skills";
+import Services from "./sass/components/services/Services";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Home />
         <About />
         <Skills />
+        <Services />
       </main>
     </div>
   );
